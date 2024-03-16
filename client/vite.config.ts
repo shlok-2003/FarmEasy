7d7f557a4c2afb4921b0a28e5012d1d9c33e6067
shlok-2003 <shlokprajapati2003@gmail.com>
2024-03-16 13:21:34 +0530

    Added routing

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+            "@components/core": path.resolve(
+                __dirname,
+                "./src/components/ui/core",
+            ),
+            "@components/common": path.resolve(
+                __dirname,
+                "./src/components/ui/common",
+            ),
+        },
+    },
+});
