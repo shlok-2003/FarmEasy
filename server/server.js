778da6a353c6fe51e779bcb5acf48faba236490f
Shlok Prajapati <shlokprajapati2003@gmail.com>
2024-09-24 18:30:30 +0530

    updated the code

--- server/server.js
+++ server/server.js
@@ -7,13 +7,12 @@ dotenv.config();
 
 const DB = process.env.DB_URI;
 
-mongoose.connect(DB).then((con) => {
-  console.log("DB CONNECTION DONE :)");
-});
-
 // SERVER START :
 // console.log("MODE (server.js) : ", .NODE_ENV);
 const port =  5001;
-app.listen(port, () => {
+app.listen(port, async() => {
+  await mongoose.connect(DB).then((con) => {
+    console.log("DB CONNECTION DONE :)");
+  });
   console.log(`Running on port - ${port}..`);
 });
